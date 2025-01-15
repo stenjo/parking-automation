1d75940be1fe04be0b137fd045b4624d180bffde
Sten Johnsen <sten@thinea.no>
2025-01-15 23:26:48 +0100

    IInitial commit

---
+++ cypress/e2e/park.cy.ts
@@ -0,0 +1,24 @@
+describe('Register parking at tastarustå', () => {
+    const username = 'Tastarustå';
+    const password = 'gjest';
+    const plate = 'ED67424'; // Replace with the actual plate number
+
+    it('should be able to register parking', () => {
+        cy.visit('https://permit.parkingguru.com/no/Account/LogIn');
+        cy.get('input[name="UserName"]').type(username);
+        cy.get('input[name="Password"]').type(password);
+        cy.get('input[type="submit"]').click(); // Log in
+        cy.get(':nth-child(2) > a').click(); // Register parking
+        cy.get('.guestWebCheckinItemHeader > :nth-child(1) > .col-md-3').should('exist');
+        cy.get('body').then($body => {
+            if ($body.find(`#ValidParking-0:contains("${plate}")`).length === 0) {
+                cy.get('#LicensePlate').type(plate);
+                cy.get('#vehicle-data-submit').click(); // Register parking
+                
+                cy.get('.modal-content').should('contain.text', 'Ved å trykke ja så er kjøretøyet registrert');
+                cy.get('.btn-primary').contains('Ja').click(); // Confirm parking
+                cy.get('#tabConent-0 > :nth-child(2) > :nth-child(2)').should('contain.text', plate);
+            }
+        });
+    });
+});
